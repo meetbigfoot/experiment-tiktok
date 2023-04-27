38c25e08ef6cf3334f2a6335186f66b1bb42ebc9
Matt Born <mattborn@users.noreply.github.com>
2023-04-26 23:05:57 -0600

    The video? It plays.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,52 @@
 const g = document.getElementById.bind(document)
 
 const api = 'https://us-central1-samantha-374622.cloudfunctions.net/tiktok'
+// const api = 'http://localhost:8080/'
 
-const tik = async query => {
-  console.log(`Looking up videos on TikTok for ${query}`)
+const search = async query => {
+  g('status').textContent = `Searching TikTok videos for ${query}…`
   const response = await fetch(api, {
     method: 'POST',
-    body: query,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      endpoint: 'search',
+      options: {
+        category: 'videos',
+        query: query,
+      },
+    }),
   })
   return response.json()
 }
 
+const video = async item => {
+  g('status').textContent = `Attempting to play TikTok video with ID ${item.id}…`
+  const response = await fetch(api, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      endpoint: 'video',
+      options: {
+        downloadAddr: item.video.downloadAddr,
+        id: item.id,
+      },
+    }),
+  })
+  const json = await response.json()
+  console.log(json)
+  const video = document.createElement('video')
+  video.autoplay = true
+  video.src = json.url
+  g('status').appendChild(video)
+}
+
 const go = () => {
   g('videos').textContent = ''
-  tik(g('input').value).then(data => {
+  search(g('input').value).then(data => {
     data.item_list.forEach(item => {
+      console.log(item)
       const post = document.createElement('div')
       post.className = 'post'
-      const video = document.createElement('video')
-      video.src = item.video.playAddr
-      post.appendChild(video)
       const cover = document.createElement('img')
       cover.src = item.video.cover
       post.appendChild(cover)
@@ -29,7 +56,10 @@ const go = () => {
       const desc = document.createElement('p')
       desc.textContent = `@${item.desc}`
       post.appendChild(desc)
+      post.onclick = () => download(item)
       g('videos').appendChild(post)
     })
   })
 }
+
+const download = item => video(item)
